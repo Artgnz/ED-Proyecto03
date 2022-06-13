c4c97c6b29e0011a8050825b360690826d71cbbc
Artgnz <arturognz@ciencias.unam.mx>
2022-06-13 01:45:11 -0500

    Documentación y formato.

--- src/Serializador.java
+++ src/Serializador.java
@@ -1,39 +1,51 @@
 package src.edd;
 
-import java.io.FileOutputStream;
 import java.io.FileInputStream;
-import java.io.ObjectOutputStream;
+import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
+/**
+ * Serializa y deserializa objetos.
+ * 
+ * @author Arturo González Peñaloza
+ * @author Emilio Arsenio Raudry Rico
+ * @author Fernanda Ameyalli Osorio Morales
+ */
 public class Serializador {
+
+    /**
+     * Dado un nombre de archivo y un objeto, serializa el objeto creando un archivo
+     * con el nombre de archivo dado.
+     * 
+     * @param nombreDeArchivo nombre del archivo.
+     * @param objeto          objeto que se serializa.
+     * @return {@code true} si fue posible serializar el objeto.
+     */
     static boolean serializar(String nombreDeArchivo, Object objeto) {
-        try {   
-            FileOutputStream archivo = new FileOutputStream(nombreDeArchivo);
-            ObjectOutputStream salida = new ObjectOutputStream(archivo);
-              
+        try (FileOutputStream archivo = new FileOutputStream(nombreDeArchivo);
+                ObjectOutputStream salida = new ObjectOutputStream(archivo)) {
             salida.writeObject(objeto);
             salida.writeObject(objeto);
-              
-            salida.close();
-            archivo.close();
             return true;
-        }
-        catch(Exception ex) {
+        } catch (Exception ex) {
             return false;
-        }   
+        }
     }
 
+    /**
+     * Dado el nombre de archivo, recrea ese objeto.
+     * 
+     * @param nombreDeArchivo nombre del archivo.
+     * @return {@code Object} objeto que se recreó a partir del archivo. Si no fue
+     *         posible recrearlo, regresa null.
+     */
     static Object deSerializar(String nombreDeArchivo) {
-        try {   
-            FileInputStream archivo = new FileInputStream(nombreDeArchivo);
-            ObjectInputStream entrada = new ObjectInputStream(archivo);
-
+        try (FileInputStream archivo = new FileInputStream(nombreDeArchivo);
+                ObjectInputStream entrada = new ObjectInputStream(archivo)) {
             Object object = entrada.readObject();
-              
-            archivo.close();
-            entrada.close();
             return object;
-        } catch(Exception ex){
+        } catch (Exception ex) {
             return null;
         }
     }
--- src/Usuarios.java
+++ src/Usuarios.java
@@ -3,16 +3,32 @@ package src.edd;
 import java.io.Serializable;
 import java.util.NoSuchElementException;
 
-
-import src.edd.Usuario;
-
+/**
+ * Almacena a usuarios.
+ * 
+ * @author Arturo González Peñaloza
+ * @author Emilio Arsenio Raudry Rico
+ * @author Fernanda Ameyalli Osorio Morales
+ */
 public class Usuarios implements Serializable {
+    /**
+     * Lista de usuarios almacenados.
+     */
     private Lista<Usuario> usuarios;
 
+    /**
+     * Constructor.
+     */
     public Usuarios() {
         usuarios = new Lista<>();
     }
 
+    /**
+     * Busca a un usuario en la lista.
+     * 
+     * @param nombreUsuario nombre del usuario.
+     * @return <tt>true</tt> si el usuario está en la lista.
+     */
     public boolean contieneUsuario(String nombreUsuario) {
         for (Usuario usuario : usuarios) {
             if (usuario.getNombreUsuario().equals(nombreUsuario)) {
@@ -22,6 +38,12 @@ public class Usuarios implements Serializable {
         return false;
     }
 
+    /**
+     * Dado el nombre de un usuario, lo regresa. ®param nombre nombre del usuario
+     * 
+     * @return {@code Usuario} usuario que se encontró.
+     * @throws NoSuchElementException si no se encuentra el nombre en la colección.
+     */
     public Usuario getUsuario(String nombre) {
         for (Usuario usuario : usuarios) {
             if (usuario.getNombreUsuario().equals(nombre)) {
@@ -30,11 +52,14 @@ public class Usuarios implements Serializable {
         }
         throw new NoSuchElementException();
     }
-    public void agregaUsuario(Usuario usuario) {
-        System.out.println("Se agregó a " + usuario);
 
+    /**
+     * Agrega a un usuario a la colección.
+     * 
+     * @param Usuario usuario a agregar.
+     */
+    public void agregaUsuario(Usuario usuario) {
         usuarios.add(usuario);
     }
-
 
 }
